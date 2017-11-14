e67c86929dbb465b068ad9a631dd697d3798d996
Josh Thorsson <josh.thorsson@gmail.com>
2017-11-14 13:17:21 -0600

    Removed some old files I created, and remade the GUI with Javafx
    Added packages bba, bba.model, bba.view.  Remade the GUI using grids and
    vbox, to make it easier to align things.  Added canvas for pixel
    display, and created two text fields.

---
+++ BitBlockProject/src/bba/MainApp.java
@@ -0,0 +1,73 @@
+package bba;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class MainApp extends Application 
+{
+	
+	private Stage primaryStage;
+	private BorderPane rootLayout;
+
+	@Override
+	public void start(Stage primaryStage) 
+	{
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("Bit Block Analyzer");
+		this.primaryStage.setResizable(false);
+		
+		initRootLayout();
+		showBitBlockGUI();
+		
+	}
+	
+	public void initRootLayout() 
+	{
+		try 
+		{
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
+			rootLayout = (BorderPane) loader.load();
+			
+			Scene scene = new Scene(rootLayout);
+			primaryStage.setScene(scene);
+			primaryStage.show();			
+		} 
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public void showBitBlockGUI()
+	{
+		try
+		{
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/BitBlockGUI.fxml"));
+			AnchorPane bitBlockGUI = (AnchorPane) loader.load();
+			
+			rootLayout.setCenter(bitBlockGUI);
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public Stage getPrimaryStage()
+	{
+		return primaryStage;
+	}
+
+	public static void main(String[] args) 
+	{
+		launch(args);
+	}
+}
