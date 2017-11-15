8dd7ba7409b2416cb42dbac7bb1042a96d143336
irfan syed <irfansyed927@gmail.com>
2017-11-14 22:08:12 -0600

    Close is finally working from the menu

--- BitBlockProject/src/bba/view/RootLayoutController.java
+++ BitBlockProject/src/bba/view/RootLayoutController.java
@@ -1,5 +1,18 @@
 package bba.view;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextArea;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+
 /*
  * @author Josh Thorsson
  */
@@ -7,5 +20,12 @@ public class RootLayoutController {
 	
 	//private Model model;
 	
-
+	@FXML
+	private MenuItem close; 
+	
+	@FXML
+	void quitAction(ActionEvent event) {
+		Platform.exit(); 
+		System.exit(0);
+	}
 }
