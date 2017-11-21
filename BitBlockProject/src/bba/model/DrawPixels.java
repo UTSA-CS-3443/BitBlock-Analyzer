07d29d21bf5da694f8ee93239e5a62cdef820588
Josh Thorsson <josh.thorsson@gmail.com>
2017-11-21 16:39:44 -0600

    Added some comments.

--- BitBlockProject/src/bba/model/DrawPixels.java
+++ BitBlockProject/src/bba/model/DrawPixels.java
@@ -14,12 +14,29 @@ import javafx.scene.image.PixelWriter;
 import javafx.scene.paint.Color;
 import javafx.scene.image.WritableImage;
 
+/*
+ * @author Josh Thorsson
+ * 
+ * DrawPixels is used to take the list of pixels obtained from SCParser, and create and image that is drawn onto the canvas
+ * 
+ */
+
 public class DrawPixels {
 	
+	/*
+	 * @param canvas The canvas we will work on to draw the BitBlock
+	 * @param width The width of each pixel
+	 * @param height The height of each pixel
+	 * @param pixelList the list of pixels obtained from SCParser.java.  Each object should contain the token and color associated with it
+	 */
 	public WritableImage drawPixels(Canvas canvas, int width, int height, List<TokenizedPixel> pixelList)
 	{
+		//Getting height/width of the canvas and converting to integer so it meets WritableImage constructor
+		int imageWidth = (int) canvas.getWidth();
+		int imageHeight = (int) canvas.getHeight();
+		
 		TokenizedPixel tPixel = new TokenizedPixel(null, null);
-		WritableImage wImage = new WritableImage(width, height);
+		WritableImage wImage = new WritableImage(imageWidth, imageHeight);
 		
 		return wImage;
 	}
