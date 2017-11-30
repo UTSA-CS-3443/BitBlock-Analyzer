04c9629b24b4a36e94eab5d786d535674ee8c9e8
hva314 <hva314@gmail.com>
2017-11-29 20:59:15 -0600

    Bitblock To PNG

--- BitBlockProject/src/parser/WritetoFile.java
+++ BitBlockProject/src/parser/WritetoFile.java
@@ -3,6 +3,11 @@ package parser;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.imageio.ImageIO;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
@@ -15,18 +20,39 @@ public class WritetoFile {
 	
 	
 	/**
-	 * @param List<String> sourcecode, String path_to_out_file
-	 * append the data to that file
 	 * 
 	 * !!! NEED: check existence of file
 	 * 			 prompt user to overwrite
 	 * 
 	 */
-	public WritetoFile(List<String> sourceCodeLines, String outfile) throws IOException
+	public WritetoFile(List<TokenizedPixel> pixelList, String outfile) throws IOException
 	{
-		this.sourceCodeLines = sourceCodeLines;
-		Path file = Paths.get(outfile);
-		Files.write(file, sourceCodeLines, Charset.forName("UTF-8"), StandardOpenOption.APPEND);
+		
+		int width = 640;
+		int height = 320;
+		
+		BufferedImage imgage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+		
+		File f = null;
+		
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				int red = (int) (Math.random() * 256); 
+				int green = (int) (Math.random() * 256); 
+				int blue = (int) (Math.random() * 256); 
+
+				int pixel = (red << 16) | (green << 8) | blue; 
+
+				imgage.setRGB(x, y, pixel);
+			}
+		}
+		
+		try {
+			f = new File(outfile + ".png");
+			ImageIO.write(imgage, "png", f);
+		} catch (IOException e) {
+			System.out.println("Error: " + e);
+		}
 	}
-	
+
 }
