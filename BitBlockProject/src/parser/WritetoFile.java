2d5082fdb13842855c7dd7d44d20f4c540879da4
hva314 <hva314@gmail.com>
2017-11-29 20:59:29 -0600

    Bitblock to PNG

--- BitBlockProject/src/parser/WritetoFile.java
+++ BitBlockProject/src/parser/WritetoFile.java
@@ -14,6 +14,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+/**
+* @author Anthony Hoang
+*/
 public class WritetoFile {
 
 	private List<String> sourceCodeLines = new ArrayList<String>();
