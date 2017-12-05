265c63da26205e8edb5ebf953def87f7f9e80b2d
hva314 <hva314@gmail.com>
2017-12-04 20:52:14 -0600

    write image to png file, 1.0

--- BitBlockProject/src/parser/WritetoFile.java
+++ BitBlockProject/src/parser/WritetoFile.java
@@ -1,59 +1,69 @@
 package parser;
 
-import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.imageio.ImageIO;
 
+import bba.model.BitBlock;
+import javafx.scene.paint.Color;
+
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 /**
 * @author Anthony Hoang
 */
 public class WritetoFile {
-
-	private List<String> sourceCodeLines = new ArrayList<String>();
-	private int side;
 	
-	/**
-	 * 
-	 * !!! NEED: check existence of file
-	 * 			 prompt user to overwrite
-	 * 
-	 */
-	public WritetoFile(List<TokenizedPixel> pixelList, String outfile) throws IOException
+	private List<Color> tColorList = new ArrayList<Color>();
+	private Color cPixel;
+	
+	public WritetoFile(String outfile, BitBlock bitblock, int scale) throws IOException
 	{
 		
-		Statistic stat = new Statistic(pixelList);
-		
-		side = stat.countPixel();
-		
-		BufferedImage imgage = new BufferedImage(side, side, BufferedImage.TYPE_INT_RGB);
-		
+		int side = bitblock.getDimension()[0];
+		BufferedImage image = new BufferedImage(side*scale, side*scale, BufferedImage.TYPE_INT_RGB);
 		File f = null;
 		
-		for (int y = 0; y < side; y++) {
-			for (int x = 0; x < side; x++) {
-				int red = (int) (Math.random() * 256); 
-				int green = (int) (Math.random() * 256); 
-				int blue = (int) (Math.random() * 256); 
+		// create array of color
+		for (int i = 0; i < bitblock.getPixelList().size(); i++)
+			tColorList.add(bitblock.getPixelList().get(i).getColor());
+		
+		// setting up image
+		int j = 0;
+		for (int y = 0; y < side; y++)
+		{
+			for (int x = 0; x < side; x++)
+			{
+				// get color 
+				if (j < tColorList.size())
+					cPixel = tColorList.get(j);
+				else // padding
+					cPixel = ColorPalette.PALETTE.get(null);
 
+				j++;
+				
+				// convert to RGB
+				int red   = (int) (cPixel.getRed() * 256);
+				int green = (int) (cPixel.getGreen() * 256);
+				int blue  = (int) (cPixel.getBlue() * 256);
+						
 				int pixel = (red << 16) | (green << 8) | blue; 
-
-				imgage.setRGB(x, y, pixel);
+				
+				// scale each pixel
+				for (int ay = y*scale; ay < (y*scale)+scale; ay++)
+					for (int ax = x*scale; ax < (x*scale)+scale; ax++)
+							image.setRGB(ax, ay, pixel);
+				
 			}
 		}
 		
+		// write to file
 		try {
 			f = new File(outfile + ".png");
-			ImageIO.write(imgage, "png", f);
+			ImageIO.write(image, "png", f);
 		} catch (IOException e) {
 			System.out.println("Error: " + e);
 		}
